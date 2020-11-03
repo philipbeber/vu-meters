ffb486449ee9d7d3f0d01e5899ce6ebade2234cf
Philip Beber <pbeber@fuze.com>
2020-11-03 08:51:39 -0800

    Fix bugs

--- src/arduino/index.ts
+++ src/arduino/index.ts
@@ -1,23 +1,28 @@
 import { SampleSet } from "../redux/reducers/audioReducer";
 
 function declareVariables(set: SampleSet, pin: number) {
-    return `int pin${pin} = ${pin};
-char pin${pin}data[] = { ${set.samples.map(sample => `0x${sample.toString(16)}`).join(", ")} };
-`
+  return `int pin${pin} = ${pin};
+char pin${pin}data[] = { ${set.samples
+    .map((sample) => `0x${sample.toString(16)}`)
+    .join(", ")} };
+`;
 }
 
 function setPinMode(set: SampleSet, pin: number) {
-    return `  pinMode(pin${pin}, OUTPUT);`
+  return `  pinMode(pin${pin}, OUTPUT);`;
 }
 
 function writePin(set: SampleSet, pin: number) {
-    return `  analogWrite(pin${pin}, pin${pin}data[index]);`
+  return `  analogWrite(pin${pin}, pin${pin}data[index]);`;
 }
 
 export function createCode(sampleSets: SampleSet[], startPin: number) {
+  const sampleCount = sampleSets[0].samples.length;
 
-const code = `
-${sampleSets.map((set, index) => declareVariables(set, startPin + index)).join("\n")}
+  const code = `
+${sampleSets
+  .map((set, index) => declareVariables(set, startPin + index))
+  .join("\n")}
 
 unsigned long startTime;
 
@@ -30,12 +35,11 @@ ${sampleSets.map((set, index) => setPinMode(set, startPin + index)).join("\n")}
 void loop() {
 
   unsigned long timeNow = millis();
-  unsigned int index = ((timeNow - startTime) / 10000) % 3;
+  unsigned int index = ((timeNow - startTime) / 100) % ${sampleCount};
 
 ${sampleSets.map((set, index) => writePin(set, startPin + index)).join("\n")}
 }
-`
+`;
 
-console.log(code);
-return code;
+  return code;
 }
